127b6647d32f6a52634fe1815fa187c715024695
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2023-11-12 03:24:27 +1000

    Update CCVars.cs

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -1181,7 +1181,7 @@ namespace Content.Shared.CCVar
         /// Whether or not to restrict the restart vote when there's online admins.
         /// </summary>
         public static readonly CVarDef<bool> VoteRestartNotAllowedWhenAdminOnline =
-            CVarDef.Create("vote.restart_not_allowed_when_admin_online", false, CVar.SERVERONLY);
+            CVarDef.Create("vote.restart_not_allowed_when_admin_online", true, CVar.SERVERONLY);
 
         /// <summary>
         ///     The delay which two votes of the same type are allowed to be made by separate people, in seconds.
