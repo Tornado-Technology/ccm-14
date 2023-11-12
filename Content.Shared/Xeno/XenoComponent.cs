244dbe29bcd11f465ac7e8e05ad961261867c385
mersen-tyn <s.letonet@gmail.com>
2023-11-12 17:19:38 +0700

    Fix.....

--- Content.Shared/Xeno/XenoComponent.cs
+++ Content.Shared/Xeno/XenoComponent.cs
@@ -5,8 +5,8 @@ namespace Content.Shared.Xeno;
 [RegisterComponent]
 public sealed partial class XenoComponent : Component
 {
-    [DataField]
-    public EntProtoId ActionNightVision = "ActionXenoNightVision";
+    // [DataField]
+    // public EntProtoId ActionNightVision = "ActionXenoNightVision";
 
     [DataField("researchPoints"), ViewVariables(VVAccess.ReadWrite)]
     public int ResearchPoints = 1000;
