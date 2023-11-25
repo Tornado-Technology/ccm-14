4f934e5a13049ab3a04e72aacd4628e2969d455d
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2023-11-25 22:31:27 +1000

    Inital

---
+++ Content.Shared/Xeno/Components/XenoRestingComponent.cs
@@ -0,0 +1,10 @@
+﻿using Content.Shared.Xeno.Systems;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Xeno.Components;
+
+[RegisterComponent, NetworkedComponent, Access(typeof(XenoRestSystem))]
+public sealed partial class XenoRestingComponent : Component
+{
+
+}
