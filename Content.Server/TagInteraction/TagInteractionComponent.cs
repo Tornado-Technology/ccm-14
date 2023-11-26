c16930e9ef52bf506aa1b8bc151124945b973ee5
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2023-11-26 14:55:22 +1000

    Added tag interaction system

---
+++ Content.Server/TagInteraction/TagInteractionComponent.cs
@@ -0,0 +1,14 @@
+﻿using Content.Shared.Tag;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server.TagInteraction;
+
+[RegisterComponent]
+public sealed partial class TagInteractionComponent : Component
+{
+    [DataField]
+    public ProtoId<TagPrototype>? WhitelistTag;
+
+    [DataField]
+    public ProtoId<TagPrototype>? BlacklistTag;
+}
