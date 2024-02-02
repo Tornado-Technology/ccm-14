b5f912d0adfd6c68ac69bf155a47b098cee9d121
Ratyyy <138193222+Ratyyy@users.noreply.github.com>
2024-02-02 13:38:14 +0200

    xeno acid

---
+++ Content.Shared/_CM14/Xenos/Acid/XenoAcidComponent.cs
@@ -0,0 +1,15 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Shared._CM14.Xenos.Acid;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(XenoAcidSystem))]
+public sealed partial class XenoAcidComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public TimeSpan AcidDelay = TimeSpan.FromSeconds(5);
+    [DataField("action", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+    public string Action = "ActionXenoAcid";
+}
