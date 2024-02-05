1fa1c7975915620abdef83f787fcde9923cea60b
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-02-05 19:16:01 +1000

    Added profession ranks

---
+++ Content.Shared/_CM14/JobRank/JobRankComponent.cs
@@ -0,0 +1,19 @@
+﻿using Content.Shared.Roles;
+using Content.Shared.StatusIcon;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._CM14.JobRank;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState, Access(typeof(SharedJobRankSystem))]
+public sealed partial class JobRankComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public ProtoId<JobPrototype>? Job;
+
+    [DataField, AutoNetworkedField]
+    public JobRankPrototype? Rank;
+
+    [DataField, AutoNetworkedField]
+    public ProtoId<StatusIconPrototype>? Icon;
+}
---
+++ Content.Shared/_CM14/JobRank/JobRankPrototype.cs
@@ -0,0 +1,34 @@
+﻿using Content.Shared.Players.PlayTimeTracking;
+using Content.Shared.Roles;
+using Content.Shared.StatusIcon;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._CM14.JobRank;
+
+[Serializable, Prototype("jobRank")]
+public sealed class JobRankPrototype : IPrototype
+{
+    [IdDataField]
+    public string ID { get; } = default!;
+
+    [DataField]
+    public ProtoId<JobPrototype> Job;
+
+    [DataField]
+    public HashSet<JobRankTimeRequirement> Requirements = new();
+}
+
+[Serializable, NetSerializable, ImplicitDataDefinitionForInheritors]
+public partial class JobRankTimeRequirement
+{
+    [DataField]
+    public ProtoId<PlayTimeTrackerPrototype> Role;
+
+    [DataField]
+    public ProtoId<StatusIconPrototype> Icon;
+
+    [DataField]
+    public TimeSpan Time;
+}
+
