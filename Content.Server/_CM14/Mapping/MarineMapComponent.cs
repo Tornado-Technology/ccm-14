131e36b9373e70e3f5540000ab4b87301c4c41e1
778b <kklokov@inbox.ru>
2024-03-08 23:18:56 +0400

    added map components

---
+++ Content.Server/_CM14/Mapping/MarineMapComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Server._CM14.Mapping;
+
+/// <summary>
+/// A piece of mech equipment that grabs entities and stores them
+/// inside of a container so large objects can be moved.
+/// </summary>
+[RegisterComponent]
+public sealed partial class MarineMapComponent : Component
+{
+}
---
+++ Content.Server/_CM14/Mapping/XenoMapComponent.cs
@@ -0,0 +1,11 @@
+namespace Content.Server._CM14.Mapping;
+
+/// <summary>
+/// A piece of mech equipment that grabs entities and stores them
+/// inside of a container so large objects can be moved.
+/// </summary>
+[RegisterComponent]
+public sealed partial class XenoMapComponent : Component
+{
+}
+
