55018204453dfc7467bdafa55e48069c5bfd03db
noverd <dasfasru@gmail.com>
2024-03-08 14:04:41 +0300

    Теперь ксено-плеваки стреляют обычной кислотой на ПКМ

--- Content.Server/_CM14/Xeno/Actions/Components/XenoSpitComponent.cs
+++ Content.Server/_CM14/Xeno/Actions/Components/XenoSpitComponent.cs
@@ -16,23 +16,6 @@ public sealed partial class XenoSpitComponent : Component
     public float Speed = 10f;
 }
 
-/// <summary>
-///     A dull crutch that allows you to cram several identical actions into one system.
-///     This MUST be remade in the future.
-/// </summary>
-[RegisterComponent]
-public sealed partial class XenoSpit2Component : Component
-{
-    [DataField("action", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
-    public string Action = "ActionDefilerDefaultSpit";
-
-    [DataField("projectile", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
-    public string Projectile = "ProjectileDefilerAcidSpit";
-
-    [DataField("speed")]
-    public float Speed = 10f;
-}
-
 [RegisterComponent]
 public sealed partial class XenoSpitRejuvenateComponent : Component
 {
