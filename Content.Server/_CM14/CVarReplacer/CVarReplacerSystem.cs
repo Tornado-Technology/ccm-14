a3de5120c9125755fde5d2fac8c249ec99945bad
noverd <dasfasru@gmail.com>
2024-03-10 13:52:18 +0300

    Hot fix

--- Content.Server/_CM14/CVarReplacer/CVarReplacerSystem.cs
+++ Content.Server/_CM14/CVarReplacer/CVarReplacerSystem.cs
@@ -5,13 +5,13 @@ namespace Content.Server._CM14.CVarReplacer;
 public sealed class CVarReplacerSystem : EntitySystem
 {
     [Dependency] private readonly IConfigurationManager _cfg = default!;
-    private const string? Url = null;
+    private const string Url = "";
     private const string CvarName = "sponsor.api_url";
 
     public override void Initialize()
     {
         base.Initialize();
-        if (_cfg.IsCVarRegistered(CvarName) && Url != null)
+        if (_cfg.IsCVarRegistered(CvarName))
         {
             _cfg.SetCVar(CvarName, Url);
         }
