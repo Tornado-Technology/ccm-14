fb46b7088c7225fa27d38bd8067670a40a93525a
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-03-23 13:05:56 +1000

    Added mortars

---
+++ Content.Client/_CM14/Mortar/MortarBui.cs
@@ -0,0 +1,53 @@
+﻿using System.Numerics;
+using Content.Shared._CM14.Mortar;
+using JetBrains.Annotations;
+
+namespace Content.Client._CM14.Mortar;
+
+[UsedImplicitly]
+public sealed class MortarBui : BoundUserInterface
+{
+    [ViewVariables]
+    private MortarMenu? _menu;
+
+    public MortarBui(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+    {
+    }
+
+    protected override void Open()
+    {
+        base.Open();
+
+        _menu = new MortarMenu(this);
+        _menu.OpenCentered();
+        _menu.OnClose += Close;
+    }
+
+    protected override void UpdateState(BoundUserInterfaceState state)
+    {
+        base.UpdateState(state);
+        if (state is not MortarBoundUserInterfaceState castState)
+            return;
+
+        _menu?.UpdateState(castState);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (!disposing)
+            return;
+
+        _menu?.Dispose();
+    }
+
+    public void Save(int index, Vector2 position)
+    {
+        SendMessage(new MortarSaveMessage(index, position));
+    }
+
+    public void Launch(int index)
+    {
+        SendMessage(new MortarLaunchMessage(index));
+    }
+}
---
+++ Content.Server/_CM14/Mortar/MortarComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using Robust.Shared.Utility;
+
+namespace Content.Server._CM14.Mortar;
+
+[RegisterComponent]
+public sealed partial class MortarComponent : Component
+{
+    [DataField]
+    public string ShellContainerId = "shell";
+
+    [DataField]
+    public int MaxSavedPositions = 3;
+
+    [DataField]
+    public List<Vector2> SavedPositions = new();
+
+    [DataField]
+    public ResPath LaunchSound = new("/Audio/Weapons/Guns/Gunshots/grenade_launcher.ogg");
+}
