71e30a470a6b9c29e06c809bc0123e63fef1d4b7
Deatherd <russianihojin@gmail.com>
2024-04-04 19:02:04 +0300

    Дополнительное изменение отображения ФФ

--- Content.Client/_CM14/FriendlyFire/FriendlyFireSystem.cs
+++ Content.Client/_CM14/FriendlyFire/FriendlyFireSystem.cs
@@ -4,6 +4,9 @@ using Content.Shared.Ghost;
 using Robust.Client.Player;
 using Robust.Shared.Prototypes;
 using Content.Shared._CM14.FriendlyFire;
+using Content.Shared.CombatMode;
+using Content.Shared.Hands.Components;
+using Content.Shared.Weapons.Ranged.Components;
 
 namespace Content.Client.FriendlyFire;
 
@@ -28,23 +31,27 @@ public sealed class FriendlyFireSystem : SharedStatusIconSystem
         if (!HasComp<FriendlyFireComponent>(ent) && !HasComp<GhostComponent>(ent))
             return;
 
+
         args.StatusIcons.Add(_prototype.Index<StatusIconPrototype>(statusIcon));
     }
 
     private void OnGetStatusIcon(EntityUid uid, FriendlyFireComponent component, ref GetStatusIconsEvent args)
     {
+        // Dont set icon if not in combat mode.
+        if (!TryComp<CombatModeComponent>(uid, out var combatMode) || !combatMode.IsInCombatMode)
+            return;
+
+        // Dont set icon if not carrying weapon
+        if (!TryComp<HandsComponent>(uid, out var hands))
+            return;
+
+        var itemInHands = hands?.ActiveHand?.HeldEntity;
+        if (itemInHands == null || !HasComp<GunComponent>(itemInHands))
+            return;
+
         if (component.Enabled)
             GetStatusIcon(component.EnableStatusIcon, ref args);
         else
             GetStatusIcon(component.DisableStatusIcon, ref args);
     }
 }
-
-
-
-
-
-
-
-
-
